ba3fd09dc1bc082bb013f10a770decb08a54a151
Samuel Hossain <samuel.hossain@outlook.com>
2024-09-03 21:12:09 -0400

    weekly challenges sets 1 and 2

--- ChallengesWithTestsMark8/ChallengesSet01.cs
+++ ChallengesWithTestsMark8/ChallengesSet01.cs
@@ -6,6 +6,10 @@ namespace ChallengesWithTestsMark8
     {
         public bool AreTwoNumbersTheSame(int num1, int num2)
         {
+            
+            return num1 == num2;
+            
+            
             if(num1 == num2)
             {
                 return true;
@@ -14,36 +18,54 @@ namespace ChallengesWithTestsMark8
             {
                 return false;
             }
+            
+            
         }
 
         public double Subtract(double minuend, double subtrahend)
         {
-            throw new NotImplementedException();
+            var answer = minuend - subtrahend;
+            return answer;
         }
 
         public int Add(int number1, int number2)
         {
-            throw new NotImplementedException();
+            return number1 + number2;
         }
 
         public int GetSmallestNumber(int number1, int number2)
         {
-            throw new NotImplementedException();
+            if (number1 < number2)
+            {
+                return number1;
+            }
+
+            return number2;
         }
 
         public long Multiply(long factor1, long factor2)
         {
-            throw new NotImplementedException();
+            return factor1 * factor2;
         }
 
         public string GetGreeting(string nameOfPerson)
         {
-            throw new NotImplementedException();
+               //if (nameOfPerson == "")
+            //   {
+           //        return "Hello!";
+           //    }
+           //    else
+           //    { 
+            //       return $"Hello!, {nameOfPerson}!";
+            //   }
+               
+               return string.IsNullOrEmpty(nameOfPerson) ? "Hello!" : $"Hello, {nameOfPerson}!";
+            
+               
         }
 
-        public string GetHey()
-        {
-            throw new NotImplementedException();
-        }
+
+        public string GetHey() => "HEY!";
     }
 }
+
